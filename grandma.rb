22fa4a52cfe58f6a0b69797bba8c5f08e99b6b28
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-15 21:07:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,2 @@
-def speak_to_grandma(hey)
-  if hey != hey.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif hey.upcase
-    return "NO, NOT SINCE 1938!"
-  if hey == hey.upcase
-    return "I LOVE YOU TOO PUMPKIN!"
-  end
-end 
+
 
